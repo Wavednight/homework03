42aa7c5d28ebfa122b9f50862cd3f8fadae38195
Mikhail <wavednight@gmail.com>
2023-12-08 17:24:53 +0700

    Задача2

---
+++ homework002/Program.cs
@@ -0,0 +1,29 @@
+﻿void FillArray(int[] array)
+{
+    Console.WriteLine("Enter elements of array:");
+    for (int index = 0; index < array.Length; index++)
+    {
+        Console.Write($"[{index}] of [{array.Length}]: ");
+        array[index] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+
+void ShowEvenNumbers(int[] array)
+{
+    int count = 0;
+
+    for (int index = 0; index < array.Length; index++)
+    {
+        if (array[index] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"The number of even numbers is {count}");
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+ShowEvenNumbers(array);
