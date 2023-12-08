9c2ff985214518a6af6b8416fc5d0c87e7a119dd
Mikhail <wavednight@gmail.com>
2023-12-08 17:26:55 +0700

    Задача3

--- homework003/Program.cs
+++ homework003/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(double[] array)
+{
+    Console.WriteLine("Enter elements of array:");
+    for (int index = 0; index < array.Length; index++)
+    {
+        Console.Write($"[{index}] of [{array.Length}]: ");
+        array[index] = Convert.ToDouble(Console.ReadLine());
+    }
+}
+
+
+void PrintRange(double[] array)
+{
+    double min = array[0];
+    double max = min;
+
+    if (array.Length < 2)
+    {
+        Console.WriteLine("Array size is too small!");
+    } else {
+
+        for (int index = 0; index < array.Length; index++)
+        {
+            if (array[index] < min)
+            {
+                min = array[index];
+            } else if (array[index] > max)
+            {
+            max = array[index];
+            }
+        }
+        Console.WriteLine("Range is: " + (max - min));
+    }
+}
+
+double[] array = new double[3];
+
+FillArray(array);
+PrintRange(array);
