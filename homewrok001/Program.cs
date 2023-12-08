fb8cbd9e4d803efd4eb2b64e43ec9ccf3fc647e2
Mikhail <wavednight@gmail.com>
2023-12-08 17:22:52 +0700

    Задача1

--- homewrok001/Program.cs
+++ homewrok001/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] array)
+{
+    Console.WriteLine("Enter elements of array:");
+    for (int index = 0; index < array.Length; index++)
+    {
+        Console.Write($"[{index}] of [{array.Length}]: ");
+        array[index] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void PrintRange(int[] mas, int min, int max)
+{
+    int count = 0;
+
+    for (int pos = 0; pos < mas.Length; pos++)
+    {
+        if ((mas[pos] >= min) && (mas[pos] <= max))
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Num of elements in range [{min}..{max}] = {count}");
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintRange(array, 20, 90);
